7e2b4e0f7741f2e3997be1b0ed4c8649da519ec2
Nguyễn Đức Đạt <ducdat612001@gmail.com>
2023-04-25 18:20:04 +0700

    update UI

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -3,6 +3,7 @@ import { NavService } from '../../service/nav.service';
 import { User } from '../../tables/user';
 import { UserService } from '../../service/user.service';
 import { FirebaseService } from '../../service/firebase.service';
+import { threadId } from 'worker_threads';
 
 @Component({
   selector: 'app-header',
@@ -16,6 +17,7 @@ export class HeaderComponent implements OnInit {
   public openNav: boolean = false;
   public isOpenMobile: boolean;
   public user: User;
+  isMobileNavOpen: boolean = false;
 
   //Log-in
   isShop: boolean = false;
@@ -44,7 +46,7 @@ export class HeaderComponent implements OnInit {
   }
 
   openMobileNav() {
-    this.openNav = !this.openNav;
+    this.isMobileNavOpen = !this.isMobileNavOpen;
   }
 
 
